026e53b5796b363dd3023808ead4eef58f1e4fd1
jieDeng <jiedeng0079@gmail.com>
2023-10-24 17:29:05 -0500

    [NOJIRA] Day 1 note

---
+++ src/main/java/org/example/day1/Day1Note.java
@@ -0,0 +1,21 @@
+package org.example.day1;
+
+public class Day1Note {
+    public static void main(String[] args) {
+/*
+    1. maven
+        folder structure
+        types of repository:
+            local (.m2), central, remote
+        maven build lifecycle
+            clean, validation, compile, test, ....
+        Command Line
+            mvn clean, mvn test, mvn install
+
+    2. git
+        distributed version control
+        how to connect with GitHub by intellij or terminal
+        basic git command
+*/
+    }
+}
